7468e978808415af3e35f9a8e5c443751c68562b
Benjamin Kuhnel <benjamin.kuhnel@epitech.eu>
2020-11-09 17:40:11 +0100

    [UPDATE] update toolbar add logout button

--- angular/src/app/components/sign-in/sign-in.component.ts
+++ angular/src/app/components/sign-in/sign-in.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../../shared/services/auth.service";
 
-import {faCoffee} from "@fortawesome/free-solid-svg-icons";
-
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-  faCoffee = faCoffee;
   constructor(
     public authService: AuthService
   ) { }
