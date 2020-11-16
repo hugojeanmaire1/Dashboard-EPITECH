4b1d6ee086e9f5d8c24bd015003da1a89ac13650
Hugo Jeanmaire <hugo.jeanmaire@epitech.eu>
2020-11-16 14:10:47 +0100

    [Adding] Spotify Service
    [Adding] Spotify Service with login, need to do API call

--- java/src/main/java/App/controller/SpotifyController.java
+++ java/src/main/java/App/controller/SpotifyController.java
@@ -1,7 +1,10 @@
 package App.controller;
 
+import com.wrapper.spotify.model_objects.credentials.AuthorizationCodeCredentials;
+import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeRequest;
+import com.wrapper.spotify.requests.data.search.simplified.SearchAlbumsRequest;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
@@ -9,45 +12,62 @@ import com.wrapper.spotify.SpotifyApi;
 import com.wrapper.spotify.SpotifyHttpManager;
 import com.wrapper.spotify.requests.authorization.authorization_code.AuthorizationCodeUriRequest;
 import java.net.URI;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/services/spotify")
-
 public class SpotifyController {
 
-    private static final String clientId = "1510f8bbdfae4bc2acd94094b2cac94e";
-    private static final String clientSecretID = "f199f9625cd9441d93fd637602c8d146";
-    private static final URI redirectUri = SpotifyHttpManager.makeUri("http:/localhost:8080/services/spotify/login/callback");
+    private static final Logger LOGGER = LoggerFactory.getLogger(TwitterController.class);
 
-    private static final SpotifyApi spotifyApi = new SpotifyApi.Builder()
-            .setClientId(clientId)
-            .setClientSecret(clientSecretID)
-            .setRedirectUri(redirectUri)
-            .build();
+    @RequestMapping(path="/login/callback")
+    public void CallbackLogin(@RequestParam String code,
+            HttpServletRequest request, HttpServletResponse response, Model model)
+    {
+        try {
+            SpotifyApi spotifyApi = (SpotifyApi)request.getSession().getAttribute("spotifyApi");
+            AuthorizationCodeRequest authorizationCodeRequest = spotifyApi.authorizationCode(code).build();
+            AuthorizationCodeCredentials authorizationCodeCredentials = authorizationCodeRequest.execute();
+            spotifyApi.setAccessToken(authorizationCodeCredentials.getAccessToken());
+            spotifyApi.setRefreshToken(authorizationCodeCredentials.getRefreshToken());
+            request.getSession().setAttribute("spotifyApi", spotifyApi);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-    private static final AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
-            .build();
-
-    public static void authorizationCodeUri_Sync() {
-        final URI uri = authorizationCodeUriRequest.execute();
-        System.out.println("URI: " + uri.toString());
     }
 
-    public static void setAuthorizationCodeUri_Async() {
-        try {
-            final CompletableFuture<URI> uriFuture = authorizationCodeUriRequest.executeAsync();
-            final URI uri = uriFuture.join();
-
-            System.out.println("URI:" + uri.toString());
-        } catch (CompletionException e) {
-            System.out.println("Error: " + e.getCause().getMessage());
-        } catch (CancellationException e) {
-            System.out.println("Async operation cancelled.");
-        }
+    @GetMapping(path="/login")
+    public RedirectView loginSpotify(HttpServletRequest request)
+    {
+        URI redirectUri = SpotifyHttpManager.makeUri("http://localhost:8080/services/spotify/login/callback");
+        SpotifyApi spotifyApi = new SpotifyApi.Builder()
+                .setClientId("7836b9f51111454685e0c44568bf67da")
+                .setClientSecret("97bd2040312b4dd499fe27d0df6cfe97")
+                .setRedirectUri(redirectUri)
+                .build();
+        AuthorizationCodeUriRequest authorizationCodeUriRequest = spotifyApi.authorizationCodeUri()
+                .show_dialog(true)
+                .build();
+        request.getSession().setAttribute("spotifyApi", spotifyApi);
+        return new RedirectView(String.valueOf(authorizationCodeUriRequest.execute()));
     }
 }
+
+/*
+        try {
+            Twitter twitter = DAO.getTwitter();
+            String callbackUrl = "http://localhost:8080/services/twitter/login/callback";
+            RequestToken requestToken = twitter.getOAuthRequestToken(callbackUrl);
+            request.getSession().setAttribute("requestToken", requestToken);
+            request.getSession().setAttribute("twitter", twitter);
+            twitterUrl = requestToken.getAuthorizationURL();
+        } catch (Exception e) {
+            LOGGER.error("Problem logging in with Twitter!", e);
+        }
+
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl(twitterUrl);
+        return redirectView;
+ */
