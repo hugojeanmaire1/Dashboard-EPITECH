552a5ca739ac5ac7ba2d7a46f73110d9e00eb7b0
Benjamin Kuhnel <benjamin.kuhnel@epitech.eu>
2020-11-19 16:15:03 +0100

    [FIX] fix merge

--- java/src/main/java/App/controller/TwitterController.java
+++ java/src/main/java/App/controller/TwitterController.java
@@ -25,7 +25,7 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 @RequestMapping("/services/twitter")
 public class TwitterController extends HttpServlet {
 
