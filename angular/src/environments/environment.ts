f9c4ccba90a83e6ee4b84679351ece78ce31a6a3
Hugo <33543525+hugojeanmaire1@users.noreply.github.com>
2020-12-01 11:24:44 +0100

    Update environment.ts

--- angular/src/environments/environment.ts
+++ angular/src/environments/environment.ts
@@ -7,16 +7,6 @@
  */
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyDqNe_dHQiGdj5RIQNFABUh-Di9B_XXZXE",
-    authDomain: "dashboard-eb9b3.firebaseapp.com",
-    databaseURL: "https://dashboard-eb9b3.firebaseio.com",
-    projectId: "dashboard-eb9b3",
-    storageBucket: "dashboard-eb9b3.appspot.com",
-    messagingSenderId: "865522504855",
-    appId: "1:865522504855:web:1c85818feeb6d22171c5d2",
-    measurementId: "G-B9212XMF4C"
-  },
 };
 
 /*
